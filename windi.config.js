736a22f8cb19ff338bc6c5e968e0c320ee768b52
DESKTOP-UISCPBM\mathew <kingdun3284@gmail.com>
2021-06-11 19:24:13 +0800

    Update package-lock.json, pnpm-lock.yaml, and windi.config.js

---
+++ windi.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extract: {
+    extractors: [
+      {
+        extractor: (content) => {
+          const classes = (content.match(/{`[^]+`}|"[^"]+"|'[^']+'/g) ?? []).map((str) => str.replace(/`|"|{|}|'/g, ""))
+          return { classes: classes }
+        },
+        extensions: ['tsx'],
+      },
+    ],
+  }
+};
