bc8d0d1ed726bfc3ad6819e4e015c363a81e73f1
DESKTOP-UISCPBM\mathew <kingdun3284@gmail.com>
2021-06-14 21:11:30 +0800

    Update cdp-menu-list.tsx, page-menu-list.tsx, and app.css

--- src/components/cdp/cdp-menu-list/cdp-menu-list.tsx
+++ src/components/cdp/cdp-menu-list/cdp-menu-list.tsx
@@ -3,67 +3,67 @@ import { deepAssign } from '../../../utils/deep-assign';
 import { CdpMenuListConfig, CdpMenuListProps } from './cdp-menu-list.interface';
 import { pathToRegexp } from 'path-to-regexp'
 @Component({
-    tag: 'cdp-menu-list',
+  tag: 'cdp-menu-list',
 })
 export class CdpMenuList {
-    defaultConfig: CdpMenuListConfig = {
-        menuItems: [],
-        anchorPropsFn: null,
-        classList: {
-            host: 'grid grid-cols-1 gap-y-2 auto-rows-min',
-            menuItem: 'py-2 px-4 hover:bg-light-blue-600 hover:text-white rounded-md grid grid-cols-[max-content,1fr,max-content] items-center gap-4 cursor-pointer',
-            menuItemActive: 'py-2 px-4 bg-light-blue-600 text-white rounded-md grid grid-cols-[max-content,1fr,max-content] items-center gap-4 cursor-pointer',
-            subMenuItem: 'py-1 px-4 hover:bg-light-blue-600 hover:text-white rounded-md grid grid-cols-[max-content,1fr,max-content] items-center gap-4',
-            subMenuItemActive: 'py-1 px-4 bg-light-blue-600 text-white rounded-md grid grid-cols-[max-content,1fr,max-content] items-center gap-4',
-            subMenuWrapper: 'grid grid-cols-1 gap-y-1 mt-2 pl-4',
-        },
-    };
-    @Element() rootEl: HTMLCdpMenuListElement;
-    @Prop() props!: CdpMenuListProps;
-    @Prop() config!: CdpMenuListConfig;
-    @State() _config: CdpMenuListConfig;
-    componentWillLoad() {
-        this._config = deepAssign(this.config, this.defaultConfig);
-    }
-    isActive(href: string, activePath: string) {
-        const regex = pathToRegexp(href);
-        if (activePath.match(regex)) return true;
-        return false
-    }
-    render() {
-        const { classList, anchorPropsFn, menuItems } = this._config;
-        const activePath = this.props.activePath;
-        return (
-            <Host class={classList.host}>
-                {menuItems.map(({ name, isActive, icon, indicator, ...props }) => {
-                    let href = props['href'];
-                    let expand = props['expand'];
-                    let active = isActive ? isActive(activePath) : this.isActive(href, activePath);
-                    if (props['nested'])
-                        return <cdp-accordion props={{ display: expand ? expand : active }}>
-                            <a slot="accordion" class={active ? classList.menuItemActive : classList.menuItem}>
-                                {icon ? icon(active) : ''}
-                                {name}
-                                {indicator ? indicator(active) : ''}
-                            </a>
-                            <div class={classList.subMenuWrapper}>
-                                {props['nested'].map(({ name, href, indicator, icon, isActive }) => {
-                                    let active = isActive ? isActive(activePath) : this.isActive(href, activePath);
-                                    return <a {...anchorPropsFn(href)} class={active ? classList.subMenuItemActive : classList.subMenuItem}>
-                                        {icon ? icon(active) : ''}
-                                        {name}
-                                        {indicator ? indicator(active) : ''}
-                                    </a>
-                                })}
-                            </div>
-                        </cdp-accordion>
-                    return <a {...anchorPropsFn(href)} class={active ? classList.menuItemActive : classList.menuItem}>
-                        {icon ? icon(active) : ''}
-                        {name}
-                        {indicator ? indicator(active) : ''}
-                    </a>
+  defaultConfig: CdpMenuListConfig = {
+    menuItems: [],
+    anchorPropsFn: null,
+    classList: {
+      host: 'grid grid-cols-1 gap-y-2 auto-rows-min',
+      menuItem: 'py-2 px-4 hover:bg-light-blue-600 hover:text-white rounded-md grid grid-cols-[max-content,1fr,max-content] items-center gap-4 cursor-pointer',
+      menuItemActive: 'py-2 px-4 bg-light-blue-600 text-white rounded-md grid grid-cols-[max-content,1fr,max-content] items-center gap-4 cursor-pointer',
+      subMenuItem: 'py-1 px-4 hover:bg-light-blue-600 hover:text-white rounded-md grid grid-cols-[max-content,1fr,max-content] items-center gap-4',
+      subMenuItemActive: 'py-1 px-4 bg-light-blue-600 text-white rounded-md grid grid-cols-[max-content,1fr,max-content] items-center gap-4',
+      subMenuWrapper: 'grid grid-cols-1 gap-y-1 mt-2 pl-4',
+    },
+  };
+  @Element() rootEl: HTMLCdpMenuListElement;
+  @Prop() props!: CdpMenuListProps;
+  @Prop() config!: CdpMenuListConfig;
+  @State() _config: CdpMenuListConfig;
+  componentWillLoad() {
+    this._config = deepAssign(this.config, this.defaultConfig);
+  }
+  isActive(href: string, activePath: string) {
+    const regex = pathToRegexp(href);
+    if (activePath.match(regex)) return true;
+    return false
+  }
+  render() {
+    const { classList, anchorPropsFn, menuItems } = this._config;
+    const activePath = this.props.activePath;
+    return (
+      <Host class={classList.host}>
+        {menuItems.map(({ name, isActive, icon, indicator, ...props }) => {
+          let href = props['href'];
+          let expand = props['expand'];
+          let active = isActive ? isActive(activePath) : this.isActive(href, activePath);
+          if (props['nested'])
+            return <cdp-accordion props={{ display: expand ? expand : active }}>
+              <a slot="accordion" class={active ? classList.menuItemActive : classList.menuItem}>
+                {icon ? icon(active) : ''}
+                {name}
+                {indicator ? indicator(active) : ''}
+              </a>
+              <div class={classList.subMenuWrapper}>
+                {props['nested'].map(({ name, href, indicator, icon, isActive }) => {
+                  let active = isActive ? isActive(activePath) : this.isActive(href, activePath);
+                  return <a {...anchorPropsFn(href)} class={active ? classList.subMenuItemActive : classList.subMenuItem}>
+                    {icon ? icon(active) : ''}
+                    {name}
+                    {indicator ? indicator(active) : ''}
+                  </a>
                 })}
-            </Host>
-        );
-    }
+              </div>
+            </cdp-accordion>
+          return <a {...anchorPropsFn(href)} class={active ? classList.menuItemActive : classList.menuItem}>
+            {icon ? icon(active) : ''}
+            {name}
+            {indicator ? indicator(active) : ''}
+          </a>
+        })}
+      </Host>
+    );
+  }
 }
--- src/components/page/page-menu-list/page-menu-list.tsx
+++ src/components/page/page-menu-list/page-menu-list.tsx
@@ -3,9 +3,8 @@ import { Component, h, Host } from '@stencil/core';
   tag: 'page-menu-list'
 })
 export class PageMenuList {
-
   render() {
-    return <Host class="">
+    return <Host class="grid grid-cols-1 gap-4">
       <ui-doc url="/doc/cdpmenulist.md"></ui-doc>
       <ui-doc url="/typedoc/interfaces/cdpmenulistconfig.md"></ui-doc>
       <ui-doc url="/typedoc/interfaces/cdpmenulistprops.md"></ui-doc>
