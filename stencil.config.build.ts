58a777350b6fe5dc5e0d7d33612f0e8935309cb5
DESKTOP-UISCPBM\mathew <kingdun3284@gmail.com>
2021-06-16 19:31:24 +0800

    Update package.json, pnpm-lock.yaml, and stencil.config.build.ts

--- stencil.config.build.ts
+++ stencil.config.build.ts
@@ -1,29 +1,22 @@
 import { Config } from '@stencil/core';
 import { windicssStencil } from '@codeperate/stencil-windicss';
 export const config: Config = {
-	namespace: 'cdp-ui-library',
-	globalScript: 'src/global/app.ts',
-	globalStyle: 'src/global/app.css',
-	taskQueue: 'async',
-	plugins: [
-		...windicssStencil({
-			preflight: true,
-		}),
-	],
-	outputTargets: [
-		{
-			type: 'dist',
-			esmLoaderPath: '../loader',
-		},
-		{
-			type: 'dist-custom-elements-bundle',
-		},
-		{
-			type: 'docs-readme',
-		},
-		{
-			type: 'www',
-			serviceWorker: null, // disable service workers
-		},
-	],
+  namespace: 'cdp-ui-library',
+  globalScript: 'src/global/app.ts',
+  globalStyle: 'src/global/app.css',
+  taskQueue: 'async',
+  plugins: [
+    ...windicssStencil({
+      preflight: false,
+    }),
+  ],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+  ],
 };
